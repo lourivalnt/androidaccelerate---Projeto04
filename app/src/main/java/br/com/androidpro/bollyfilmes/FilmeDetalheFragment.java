02cc71470069c22a3d964cf61014d2086383a726
Lourival Linard <l.linard@gmail.com>
2020-04-01 20:41:45 -0300

    Projeto 04 - Etapa 14

--- app/src/main/java/br/com/androidpro/bollyfilmes/FilmeDetalheFragment.java
+++ app/src/main/java/br/com/androidpro/bollyfilmes/FilmeDetalheFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -44,8 +43,6 @@ public class FilmeDetalheFragment extends Fragment {
 
         RatingBar avaliacao = (RatingBar) view.findViewById(R.id.item_avaliacao);
         avaliacao.setRating(itemFilme.getAvaliacao());
-
-        Button btnTrailer = (Button) view.findViewById(R.id.item_btn_trailer);
 
         return view;
     }
